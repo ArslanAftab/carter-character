e1ebc671261f03cf3fefd2d31e08e9a03cd86b86
Arslan Aftab <arslan@Arslans-MacBook-Pro.local>
2023-10-07 14:41:06 +0100

    feat: add character creation progressive form

--- carter-character/src/App.js
+++ carter-character/src/App.js
@@ -1,24 +1,143 @@
-import React from 'react';
-import { Container, Text, Button } from '@mantine/core';
-import { notifications } from '@mantine/notifications';
+import React, { useState, useCallback } from 'react';
+import { Container, Text, Button, Textarea, TextInput, Select, Notification, Group, rem } from '@mantine/core';
+import { IconUpload, IconPhoto, IconX } from '@tabler/icons-react';
+import { Dropzone, IMAGE_MIME_TYPE } from '@mantine/dropzone';
+import { useNotifications } from '@mantine/notifications';
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [character, setCharacter] = useState({
+    description: '',
+    image: null,
+    name: '',
+    gender: '',
+    age: '',
+    voice: ''
+  });
+
+  const onDrop = useCallback((acceptedFiles) => {
+    if (acceptedFiles.length) {
+      const file = acceptedFiles[0];
+      setCharacter({ ...character, image: file });
+    }
+  }, [character]);
+
+  const renderStep = () => {
+    switch(step) {
+      case 1:
+        return (
+          <Textarea 
+            label="Describe your character" 
+            placeholder="Description" 
+            value={character.description} 
+            onChange={(event) => setCharacter({...character, description: event.target.value})} 
+            minRows={4}
+          />
+        );
+      case 2:
+        return (
+          <Dropzone
+            onDrop={onDrop}
+            accept={IMAGE_MIME_TYPE}
+            maxSize={5 * 1024 ** 2}
+          >
+            <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: 'none' }}>
+              <Dropzone.Accept>
+                <IconUpload
+                  style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-blue-6)' }}
+                  stroke={1.5}
+                />
+              </Dropzone.Accept>
+              <Dropzone.Reject>
+                <IconX
+                  style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-red-6)' }}
+                  stroke={1.5}
+                />
+              </Dropzone.Reject>
+              <Dropzone.Idle>
+                <IconPhoto
+                  style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-dimmed)' }}
+                  stroke={1.5}
+                />
+              </Dropzone.Idle>
+
+              <div>
+                <Text size="xl" inline>
+                  Drag images here or click to select files
+                </Text>
+                <Text size="sm" c="dimmed" inline mt={7}>
+                  Attach as many files as you like, each file should not exceed 5mb
+                </Text>
+              </div>
+            </Group>
+          </Dropzone>
+        );
+
+      case 3:
+        return (
+          <TextInput 
+            label="Name" 
+            placeholder="Name" 
+            value={character.name} 
+            onChange={(event) => setCharacter({...character, name: event.target.value})}
+          />
+        );
+      case 4:
+        const genders = ['Male', 'Female', 'Other'];
+        return (
+          <Select 
+            label="Gender" 
+            data={genders} 
+            value={character.gender} 
+            onChange={(val) => setCharacter({...character, gender: val})}
+          />
+        );
+      case 5:
+        return (
+          <TextInput 
+            label="Age" 
+            type="number" 
+            placeholder="Age" 
+            value={character.age.toString()} // Convert number to string
+            onChange={(event) => setCharacter({...character, age: event.target.value})}
+          />
+        );
+      case 6:
+        const voices = ['Voice 1', 'Voice 2', 'Voice 3'];
+        return (
+          <Select 
+            label="Select Voice" 
+            data={voices} 
+            value={character.voice} 
+            onChange={(val) => setCharacter({...character, voice: val})}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <Container size={400} style={{ marginTop: 50 }}>
       <Text align="center" size="xl">
-        First time using the Mantine UI Library
+        Create your character <br/>
+        Step {step}: 
       </Text>
+      {renderStep()}
+      <Button style={{ marginTop: 20 }} onClick={() => setStep(step - 1)} disabled={step === 1}>
+        Previous
+      </Button>
       <Button
-        style={{ marginTop: 20 }}
-        fullWidth
+        style={{ marginLeft: 10 }}
         onClick={() => {
-          notifications.show({
-            title: 'Hello from Mantine!',
-            message: 'You clicked the button.',
-          });
+          if(step < 6) {
+            setStep(step + 1);
+          } else {
+            // Submit form or perform next action
+          }
         }}
       >
-        Click me!
+        {step < 6 ? 'Next' : 'Submit'}
       </Button>
     </Container>
   );
--- carter-character/src/index.js
+++ carter-character/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import '@mantine/core/styles.css';
+import '@mantine/dropzone/styles.css';
 import App from "./App";
 import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
