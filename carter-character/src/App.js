7e6f46cdcb99541323276233bd06a85853dfa266
Arslan Aftab <arslan@Arslans-MacBook-Pro.local>
2023-10-07 14:59:43 +0100

    feat: improve dropzone to accept and replace images, include preview

--- carter-character/src/App.js
+++ carter-character/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useCallback } from 'react';
-import { Container, Text, Button, Textarea, TextInput, Select, Group, rem } from '@mantine/core';
-import { IconUpload, IconPhoto, IconX } from '@tabler/icons-react';
+import { Container, Text, Button, Textarea, TextInput, Select, Group, rem, Image } from '@mantine/core';
+import { IconUpload, IconPhoto, IconX, IconZoomReplace } from '@tabler/icons-react';
 import { Dropzone, IMAGE_MIME_TYPE } from '@mantine/dropzone';
-import { useNotifications } from '@mantine/notifications';
+import { notifications } from '@mantine/notifications';
 
 function App() {
   const [step, setStep] = useState(1);
@@ -16,12 +16,48 @@ function App() {
   });
 
   const onDrop = useCallback((acceptedFiles) => {
-    if (acceptedFiles.length) {
-      const file = acceptedFiles[0];
-      setCharacter({ ...character, image: file });
-    }
+    const file = acceptedFiles[0];
+    setCharacter({ ...character, image: file });
+    notifications.show({
+      title: 'Image uploaded',
+      message: 'Great! Your character has an image now.',
+    });
   }, [character]);
 
+  const removeImage = () => {
+    setCharacter({ ...character, image: null });
+  };
+
+  const renderDropzoneContent = () => {
+    if (character.image) {
+      return (
+        <Group direction="column" align="center" spacing="md">
+          <Image src={URL.createObjectURL(character.image)} width={100} height={100} fit="contain" />
+          <Button onClick={removeImage} leftSection={<IconZoomReplace />} color="red">
+            Replace Image
+          </Button>
+        </Group>
+      );
+    }
+
+    return (
+      <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: 'none' }}>
+        <Dropzone.Accept>
+          <IconUpload style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-blue-6)' }} stroke={1.5} />
+        </Dropzone.Accept>
+        <Dropzone.Reject>
+          <IconX style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-red-6)' }} stroke={1.5} />
+        </Dropzone.Reject>
+        <Dropzone.Idle>
+          <IconPhoto style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-dimmed)' }} stroke={1.5} />
+        </Dropzone.Idle>
+        <div>
+          <Text size="xl" inline>Drag images here or click to select files</Text>
+          <Text size="sm" c="dimmed" inline mt={7}>Upload your image here</Text>
+        </div>
+      </Group>
+    );
+  };
   const renderStep = () => {
     switch(step) {
       case 1:
@@ -41,38 +77,9 @@ function App() {
             accept={IMAGE_MIME_TYPE}
             maxSize={5 * 1024 ** 2}
           >
-            <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: 'none' }}>
-              <Dropzone.Accept>
-                <IconUpload
-                  style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-blue-6)' }}
-                  stroke={1.5}
-                />
-              </Dropzone.Accept>
-              <Dropzone.Reject>
-                <IconX
-                  style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-red-6)' }}
-                  stroke={1.5}
-                />
-              </Dropzone.Reject>
-              <Dropzone.Idle>
-                <IconPhoto
-                  style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-dimmed)' }}
-                  stroke={1.5}
-                />
-              </Dropzone.Idle>
-
-              <div>
-                <Text size="xl" inline>
-                  Drag images here or click to select files
-                </Text>
-                <Text size="sm" c="dimmed" inline mt={7}>
-                  Attach as many files as you like, each file should not exceed 5mb
-                </Text>
-              </div>
-            </Group>
+            {renderDropzoneContent()}
           </Dropzone>
         );
-
       case 3:
         return (
           <TextInput 
@@ -133,7 +140,7 @@ function App() {
           if(step < 6) {
             setStep(step + 1);
           } else {
-            // Submit form or perform next action
+            // TODO: Submit form or perform next action
           }
         }}
       >
