175f070ba9b53000f51c8f3305cde0c03860027e
Arslan Aftab <arslan@Arslans-MBP.broadband>
2023-10-08 13:05:22 +0100

    feat: print image metadata

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Form, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 import os
+from PIL import Image
 
 app = FastAPI()
 
@@ -22,14 +23,41 @@ async def create_character(
     # Save the uploaded image to the directory
     image_filename = os.path.join(UPLOAD_DIR, image.filename)
     with open(image_filename, "wb") as buffer:
-        buffer.write(image.file.read())
-    
-    # Here, you can process the rest of the data as needed
-    print(description, name, gender, age, voice)
+        image_content = image.file.read()
+        buffer.write(image_content)
+
+    # Get image dimensions
+    with Image.open(image_filename) as img:
+        width, height = img.size
+
+    # Extract and print image metadata
+    image_metadata = {
+        "Filename": image.filename,
+        "Content-Type": image.content_type,
+        "Size (bytes)": len(image_content),
+        "Dimensions": f"{width}x{height}"
+    }
+
+    # Neatly print the received character data
+    print(f"Received Character Information:\n"
+          f"--------------------------\n"
+          f"Name: {name}\n"
+          f"Description: {description}\n"
+          f"Gender: {gender}\n"
+          f"Age: {age}\n"
+          f"Voice: {voice}\n"
+          f"Image saved as: {image_filename}\n"
+          f"Image Metadata:\n"
+          f"\tFilename: {image_metadata['Filename']}\n"
+          f"\tContent-Type: {image_metadata['Content-Type']}\n"
+          f"\tSize (bytes): {image_metadata['Size (bytes)']}\n"
+          f"\tDimensions: {image_metadata['Dimensions']}\n"
+          f"--------------------------")
     
     return {"status": "success", "message": "Character received!"}
 
 # Setup CORS to accept requests from your React app
+# TODO: Restrict access for deployment and secure endpoints
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
