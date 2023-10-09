22a923b233ace0beb71e1f9a4a949cdb73bf3638
Arslan Aftab <arslan@Arslans-MBP.broadband>
2023-10-09 12:33:51 +0100

    colorscheming using buttons

--- src/index.js
+++ src/index.js
@@ -4,17 +4,37 @@ import "./index.css";
 import '@mantine/core/styles.css';
 import '@mantine/dropzone/styles.css';
 import App from "./App";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, Button, Group } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
+import { useMantineColorScheme } from '@mantine/core';
+import { IconSun, IconMoon } from '@tabler/icons-react';
 import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <React.StrictMode>
+function ThemeToggler() {
+  const { setColorScheme } = useMantineColorScheme();
+
+  return (
+    <Group>
+      <Button leftSection={<IconSun/>} onClick={() => setColorScheme('light')}>Light</Button>
+      <Button leftSection={<IconMoon />} onClick={() => setColorScheme('dark')}>Dark</Button>
+    </Group>
+  );
+}
+
+function Root() {
+  return (
     <MantineProvider theme={theme}>
       <Notifications />
-        <App />
+      <ThemeToggler />
+      <App />
     </MantineProvider>
+  );
+}
+
+root.render(
+  <React.StrictMode>
+    <Root />
   </React.StrictMode>
 );
